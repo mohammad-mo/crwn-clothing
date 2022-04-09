f0dbad86daabed1f8bb80f7d32383ea77f646b1a
Mohammad <mohammadmahone@gmail.com>
2022-04-09 13:01:07 +0430

    - Create navigation component and add nested routing to it.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/Navigation";
 import Home from "./routes/home/Home";
+
+const Shop = () => {
+  return <h1>Shop component</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" index element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
