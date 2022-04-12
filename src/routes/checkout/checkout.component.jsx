5df2a5f8ddfd4c4755a57daffd446f96dfc17cfb
Mohammad <mohammadmahone@gmail.com>
2022-04-12 12:20:52 +0430

    - Add total count of price.

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,32 +1,36 @@
 import React, { useContext } from 'react'
 import { CartContext } from '../../contexts/cart.context'
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+
 import './checkout.styles.scss'
 
 const Checkout = () => {
-  const { cartItems, addItemToCart, removeItemToCart } = useContext(CartContext)
+  const { cartItems, cartTotal } = useContext(CartContext)
 
   return (
-    <div>
-      <div>
-        {React.Children.toArray(
-          cartItems.map((cartItem) => {
-            const { name, quantity } = cartItem
-            return (
-              <div>
-                <h2>{name}</h2>
-                <span>{quantity}</span>
-                <br />
-                <span onClick={() => removeItemToCart(cartItem)}>
-                  decrement
-                </span>
-                <br />
-                <span onClick={() => addItemToCart(cartItem)}>increment</span>
-              </div>
-            )
-          }),
-        )}
+    <div className='checkout-container'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
       </div>
+      {React.Children.toArray(
+        cartItems.map((cartItem) => <CheckoutItem cartItem={cartItem} />),
+      )}
+      <span className='total'>Total: ${cartTotal}</span>
     </div>
   )
 }
