5aaf194b6a9f6632d790bb2b481f418c96b38476
Mohammad <mohammadmahone@gmail.com>
2022-05-10 11:33:43 +0430

    - Add a spinner when fetching data.

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 
-import { selectCategoriesMap } from '../../store/categories/categories.selector'
+import {
+  selectCategoriesMap,
+  selectIsLoading,
+} from '../../store/categories/categories.selector'
 
 import ProductCard from '../../components/product-card/product-card.component'
+import Spinner from '../../components/spinner/spinner.component'
 
 import './category.styles.scss'
 
 const Category = () => {
   const { category } = useParams()
   const categoriesMap = useSelector(selectCategoriesMap)
+  const isLoading = useSelector(selectIsLoading)
   const [products, setProducts] = useState(categoriesMap[category])
 
   useEffect(() => {
@@ -19,13 +24,19 @@ const Category = () => {
 
   return (
     <>
-      <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className='category-container'>
-        {products &&
-          React.Children.toArray(
-            products.map((product) => <ProductCard product={product} />),
-          )}
-      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h2 className='category-title'>{category.toUpperCase()}</h2>
+          <div className='category-container'>
+            {products &&
+              React.Children.toArray(
+                products.map((product) => <ProductCard product={product} />),
+              )}
+          </div>
+        </>
+      )}
     </>
   )
 }
